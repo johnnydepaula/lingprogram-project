06bb11246d8eb013d530178a0bcba3280f7beb76
johnnydepaula <johnny.dias.depaula@gmail.com>
2023-05-24 14:27:43 -0300

    adiciona metodos para adicao, remocao, verificacao e calculo de orcamento/faturamento

--- src/main/java/com/lingprogram/Historico.java
+++ src/main/java/com/lingprogram/Historico.java
@@ -1,19 +1,52 @@
 package main.java.com.lingprogram;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Historico {
-    private static ArrayList<Contrato> contratos;
+    private static List<Contrato> contratos;
 
     public Historico(){
         contratos = new ArrayList<>();
     }
 
-    public void adicionaContrato(Contrato contrato){
+    public void addContrato(Contrato contrato){
         contratos.add(contrato);
     }
 
-    public String detalhaContrato(){
-        return contratos.toString();
+    public void removeContrato(Contrato contrato){
+        contratos.remove(contrato);
+    }
+
+    public void verificaContrato(Contrato contrato){
+        contratos.contains(contrato);
+    }
+
+    public List<Contrato> getContratos(){
+        return contratos;
+    }
+
+    public String detalhaContratoHistorico(Contrato contrato){
+        return contrato.descreveContrato();
+    }
+
+    public double calculaFaturamentoPrevisto(Historico historico){
+        double valorTotal = 0;
+        List<Contrato> lista = historico.getContratos();
+
+        for(Contrato contrato : lista){
+            valorTotal += contrato.valorOrcadoEmContrato();
+        }
+        return valorTotal;
+    }
+
+    public double calculaFaturamentoPago(Historico historico){
+        double valorTotal = 0;
+        List<Contrato> lista = historico.getContratos();
+
+        for(Contrato contrato : lista){
+            valorTotal += contrato.valorPagoEmContrato();
+        }
+        return valorTotal;
     }
 }
